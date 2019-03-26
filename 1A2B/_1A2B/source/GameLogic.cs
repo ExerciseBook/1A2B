9164003c5723ce0fe52a06e610f8ac64e90d1d3d
Eric_Lian <ExerciseBook@users.noreply.github.com>
2019-03-26 19:38:47 +0800

    完善游戏逻辑框架

--- 1A2B/_1A2B/source/GameLogic.cs
+++ 1A2B/_1A2B/source/GameLogic.cs
@@ -1,4 +1,6 @@
-﻿namespace _1A2B.source
+﻿using System;
+
+namespace _1A2B.source
 {
     public class GameLogic
     {
@@ -8,19 +10,24 @@
 
         public struct Response//the response of submit 
         {
-            public int nA, nB;//number of A&B
+            public int n, nA, nB;//number of A&B
         };
 
+        /// 用来储存用户的历史提交记录
+        public Response[] stats = new Response[10];
+
+
         private Response ans;
-        /*
-         * 本方法用来判断这个提交是否合法
+
+        /** 本方法用来判断这个提交是否合法
+         * 
          * 根据1A2B的游戏规则，用户提交的四位数必须互不相等，并且不能有前导零
          *
          * @param a 需要判断你的数字
          * 
-         * @return true 合法 , false 不合法
+         * @return (true 合法 , false 不合法)
          */
-        public bool isValid(int a)
+        public bool IsValid(int a)
         {
             int[] p = new int[10];
 
@@ -41,10 +48,18 @@
         }
 
 
-        public bool start(int a)
+        /** 本方法用于进行游戏初始化
+         * 
+         * 在游戏开始时本方法会被调用
+         */
+        public void Start()
         {
-            //游戏开始的时候这个方法会被调用
-            //返回值啥的我没有限定，反正你最后告诉我你返回值是啥意思就好
+            //所有变量的初始化在这里都要丢一份
+
+            int a = 0;
+            Random rand = new Random();
+            while (!IsValid(a)) { a = rand.Next(10000); };
+
             msnTargetUnitsDigit = a % 10; a /= 10;
             msnTargetTensDigit = a % 10; a /= 10;
             msnTargetHundredsDigit = a % 10; a /= 10;
@@ -54,31 +69,26 @@
             aTargetNumber[msnSubmitTensDigit] = true;
             aTargetNumber[msnTargetHundredsDigit] = true;
             aTargetNumber[msnTargetThousandsDigit] = true;
-
-            if (isValid(a))
-                return true; //true -> can start games normally
-            else
-                return false; //false -> cannot start games       
         }
 
 
-        public void finish()
+        /** 本方法用于获取游戏当前状态
+         * 
+         * @return (0 游戏未开始, 1 正在游戏, 2 游戏结束)
+         * 
+         */ 
+        public int GetGameStatus()
         {
-            /*
-             *我不知道这里应该写啥，输出表示胜利的message吗
-             */
+            throw new Exception("待补充");
+            return 0;
         }
 
 
-        public bool isfinish()
-        {
-            if (ans.nA == 4)
-                return true;//true -> finished
-            else
-                return false;//false -> notfinished
-        }
-
-        public void judge(int a)
+        /** 提交信息判定
+         * 
+         * @param a 待判定数值
+         */
+        private void Judge(int a)
         {
             //diverse the number
             msnSubmitUnitsDigit = a % 10; a /= 10;
@@ -120,26 +130,21 @@
         }
 
 
-
+        /** 本方法用于提交用户输入的四位数
+         * 
+         * @param a 用户提交的四位数
+         * 
+         * @return (0 正常处理, -1 用户输入不合法)
+         */ 
         public int submit(int a)
         {
             //用户提交答案尝试的时候这个方法会被调用
             //返回值啥的我没有限定，反正你最后告诉我你返回值是啥意思就好
 
-            if (isValid(a))
+            if (IsValid(a))
             {
-                judge(a);
-                if (isfinish())
-                {
-                    finish();
-                    return 1;//1 -> win
-                }
-                else
-                    return 0;//0 -> lost
-                /*
-                 * 
-                 * 
-                 */
+                Judge(a);
+                return 0;
             }
             else
                 return -1;//-1 -> abnormal exit(by illegal input)
